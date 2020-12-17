d32a54a0a03dfdfb8c99b04246323a998806d7ca
Kevin Chu <kvnchu2@gmail.com>
2020-12-17 06:35:10 +0000

    added in error messages for no characters and characters exceeding

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,11 +10,50 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(()=> {
   
+  $("#tweet-button").on('click', function (event) {
+    event.preventDefault();
+    
+    $textfield = $(this).closest("form").find("#tweet-text");
+    $counter = $(this).closest("form").find(".counter");
+    $textdata = $textfield.serialize();
+    $text = $textfield.val().trim();
+
+    $lengthOfText = $text.length;
+
+    if ($text === '' || $text === null) {
+      $( ".display-hidden" ).slideDown( "slow" );
+      $(".error").text("Please enter a tweet!");
+    } else if ($lengthOfText > 140) {
+      $( ".display-hidden" ).slideDown( "slow" );
+      $(".error").text("Your tweet exceeded character limit!");
+    } else {
+      $(".display-hidden").hide();
+      $.post("/tweets/", $textdata)
+        .done(function () {
+          loadTweets();
+        });
+    }
+  });
+
+  const loadTweets = () => {
+    $.getJSON("/tweets")
+      .done(function (tweets) {
+        renderTweets(tweets);
+      });
+  };
+
+  loadTweets();
+});
 
 const createTweetElement = function(tweet) {
   const currentTime = new Date();
   const dateOfTweet = new Date(parseInt(tweet.created_at))
   let timeDiff = Math.floor((currentTime - dateOfTweet) / 86400000);
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
   return `<article class="tweet-article">
           <header class="tweet-header">
             <div class="avatar-name">
@@ -25,7 +64,7 @@ const createTweetElement = function(tweet) {
               <p><strong>${tweet.user.handle}</strong></p>
             </div>
           </header>
-          <p>${tweet.content.text}</p>
+          <p class= "tweet-message">${escape(tweet.content.text)}</p>
           <footer>
             <div class="tweet-time">
               <p>${timeDiff} days ago</p>
@@ -41,30 +80,14 @@ const createTweetElement = function(tweet) {
 
   const renderTweets = function(tweets) {
     // loops through tweets
-    for (let tweet of tweets) {
-      // calls createTweetElement for each tweet
-      const $tweet = createTweetElement(tweet)
-      // takes return value and appends it to the tweets container
+    $('.tweet-container').empty();
+    for (let x = tweets.length - 1; x >= 0; x--) {
+      
+      const $tweet = createTweetElement(tweets[x]);
+      
       $('.tweet-container').append($tweet);
     }
   }
-  const loadTweets = () => {
-    $.getJSON("/tweets")
-      .done(function (tweets) {
-          renderTweets(tweets);
-          });
-  };
-  const $form = $('#tweet-form');
-  $form.submit(function (event) {
-    event.preventDefault();
 
-    console.log('Button clicked, performing ajax call...');
-    const text = $(this).serialize();
-    $.ajax({type: 'POST', url: '/tweets/', data: text})
-    .then(function () {
-      loadTweets();
-    }
-    );
-  });
-});
+
 
